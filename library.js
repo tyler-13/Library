dfa84982889965b1bbe0e6e601da0617e1eeee8c
timoe13 <tyler.matthiasmagri@gmail.com>
2023-12-23 15:01:24 -0800

    adding/removing books in html

--- library.js
+++ library.js
@@ -20,20 +20,37 @@ function createBook(title, author, pages, read) {
     div.innerHTML = `
     <div class="book-info">
         <p class="title">${title}</p>
-        <p class="author">${author}</p>
+        <p class="author">By. ${author}</p>
         <p class="pages">${pages} pages</p>
+        <button class="remove-book">Remove</button>
     </div>
     `;
     const button = document.createElement('button');
+    button.addEventListener('click' , () => {
+        if (button.className === 'has-read book-btn'){
+            button.className = 'has-not-read book-btn';
+            button.textContent = 'Has Not Read';
+        } else {
+            button.className = 'has-read book-btn';
+            button.textContent = 'Has Read';
+        }
+    })
     if (read) {
-        button.className = 'has-read';
+        button.className = 'has-read book-btn';
         button.textContent = "Has Read";
     } else {
-        button.className = 'has-not-read';
+        button.className = 'has-not-read book-btn';
         button.textContent = "Has Not Read";
     }
     div.appendChild(button);
     bookSets.push(new BookSet(new Book(title, author, pages, read), div));
+    const remove = div.querySelector('button.remove-book');
+    remove.addEventListener('click', () => {
+        const div = button.parentElement;
+        //const index = bookSets.findIndex(div);
+        div.parentElement.removeChild(div);
+        //bookSets.splice(index, 1);
+    })
     bookContainer.appendChild(div);
     console.log("TEST");
     console.log(bookSets);
@@ -57,12 +74,28 @@ addBook.addEventListener('click', () => {
 const submit = document.querySelector('.submit');
 const bookContainer = document.querySelector('.book-container');
 submit.addEventListener('click', () => {
-    const title = document.querySelector('title').textContent;
-    const author = document.querySelector('author').textContent;
-    const pages = document.querySelector('pages').textContent;
+    const title = document.querySelector('input.title').value;
+    const author = document.querySelector('input.author').value;
+    const pages = document.querySelector('input.pages').value;
     const read = document.querySelector('#has-read').checked;
-    console.log(`${title} , ${author} , ${pages} , ${read}`);
-
+    const notRead = document.querySelector('#has-not-read').checked;
+    if (title === '' || author === '' || pages === '' || (read == false && notRead === false)) {
+        return;
+    } else {
+        document.querySelector('input.title').value = '';
+        document.querySelector('input.author').value = '';
+        document.querySelector('input.pages').value = '';
+        if (read) {
+            document.querySelector('#has-read').checked = false
+        } else {
+            document.querySelector('#has-not-read').checked = false
+        }
+        console.log (title + " " + author + " " + pages + " " + read);
+        createBook(title, author, pages, read);
+        addBookForm.style.display = 'none';
+        main.style.display = 'flex';
+    }
+    
 })
 
 
@@ -70,8 +103,3 @@ submit.addEventListener('click', () => {
 
 
 
-const bookBtns = document.querySelectorAll('.book-btn');
-
-bookBtns.forEach((button) => {
-    button.addEventListener
-})
